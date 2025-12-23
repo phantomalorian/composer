8571b6702efbcbc606fa2ccb6b2e5cea87148aba
phantomalorian <delirius2017@yandex.ru>
2025-12-23 21:02:21 +0300

    the structure was changed and paths to pages were added to the main controller

--- project/app/Http/Controllers/MainController.php
+++ project/app/Http/Controllers/MainController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function catalog()
+    {
+        return view('catalog');
+    }
+
+    public function support()
+    {
+        return view('support');
+    }
 }
--- project/routes/web.php
+++ project/routes/web.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+use App\Http\Controllers\MainController;
 
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/catalog', function () {
-    return view('catalog');
-});
-Route::get('/support', function () {
-    return view('support');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/about', [MainController::class, 'about']);
+Route::get('/catalog', [MainController::class, 'catalog']);
+Route::get('/support', [MainController::class, 'support']);
 
-Route::get('/user/{id}/{name}', function ($id, $name) {
-    return 'ID:' . $id . '.Name:' . $name;
-});
+// Route::get('/catalog', function () {
+//     return view('catalog');
+// });
+// Route::get('/support', function () {
+//     return view('support');
+// });
+
+// Route::get('/user/{id}/{name}', function ($id, $name) {
+//     return 'ID:' . $id . '.Name:' . $name;
+// });
